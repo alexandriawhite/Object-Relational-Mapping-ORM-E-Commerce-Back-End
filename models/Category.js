da0257bb7fb4502436dc7352ef92e9d6885ecd62
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-18 18:02:38 -0600

    Ran npm install, ran schema.sql, added catetegory.init columns

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    //Example from 26-STU_Literals
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
